90cbc3a0599848ff24cb4d0a10f9edbae8c48948
JATIN12SETHI <500087857@stu.upes.ac.in>
2024-04-21 02:18:35 +0530

    commit

--- app.py
+++ app.py
@@ -1,10 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+Jobs=[
+    {
+        'id': 1,
+        'title': 'Software Developer',
+        'company': 'Google',
+        'location': 'Mountain View, CA',
+        'salary': '20 LPA',
+        'description': 'Develop software for Google'
+    },
+    {
+        'id': 2,
+        'title': 'Junior Developer',
+        'company': 'Google',
+        'location': 'Mountain View, CA',
+        'salary': '30 LPA',
+        'description': 'Develop software for Google'
+    },
+    {
+        'id': 3,
+        'title': 'Senior Analyst Developer',
+        'company': 'Google',
+        'location': 'Mountain View, CA',
+        'salary': '40 LPA',
+        'description': 'Develop software for Google'
+    }
+]
+
 @app.route('/')
+
 def hello_world():
-    return 'Hello, World Jatin!'
+    return render_template('home.html', jobs=Jobs,app_name='Job Board')
 
 if __name__ == '__main__':
     app.run()
